047d82de2a44b9109f37404ca6cbfcbd0f45faf3
Tiago Gauziski <tiago.gauziski@gmail.com>
2024-02-18 14:34:29 +1300

    add entity framework.

---
+++ Expense.Tracking.Api.Tests/DatabaseTests.cs
@@ -0,0 +1,16 @@
+﻿using Expense.Tracking.Api.Infrastrucure.Database;
+
+namespace Expense.Tracking.Api.Tests;
+
+public class DatabaseTests
+{
+    [Fact]
+    public async Task SaveDataIntoDatabase()
+    {
+        var context = new ExpenseContext();
+
+        // Ensure database gets created
+        await context.Database.EnsureCreatedAsync();
+
+    }
+}
---
+++ Expense.Tracking.Api/Infrastrucure/Database/ExpenseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense.Tracking.Api.Infrastrucure.Database
+{
+    public class ExpenseContext : DbContext
+    {
+        public DbSet<Domain.Transaction> Transactions { get; set; }
+
+        public string DbPath { get; }
+
+        public ExpenseContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "expenses.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
