ad449bb4b42737d5131fd8f4becf7ccb469d9171
Tiago Gauziski <tiago.gauziski@gmail.com>
2024-02-27 22:12:09 +1300

    adding category list component

--- src/expense-tracking-ui/src/app/header/header.component.ts
+++ src/expense-tracking-ui/src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
