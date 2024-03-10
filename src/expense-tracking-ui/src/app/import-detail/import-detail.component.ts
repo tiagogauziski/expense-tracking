70550b7d1f505558afb4db33f7562fefb2cbde3c
Tiago Gauziski <tiago.gauziski@gmail.com>
2024-03-10 21:56:33 +1300

    display transactions on import detail

--- src/expense-tracking-ui/src/app/import-detail/import-detail.component.ts
+++ src/expense-tracking-ui/src/app/import-detail/import-detail.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatOption } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelect } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ImportService } from '../services/import.service';
+import { Import } from '../models/import.model';
+import { MatTableModule } from '@angular/material/table';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-import-detail',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButton, MatSelect, MatOption, MatTableModule, DatePipe, CurrencyPipe],
   templateUrl: './import-detail.component.html',
   styleUrl: './import-detail.component.css'
 })
 export class ImportDetailComponent {
+  importId: string = "";
+  importModel?: Import = undefined;
+  importForm = new FormGroup({
+    name: new FormControl('', []),
+    layout: new FormControl('', []),
+    createdAt: new FormControl('', []),
+  });
+  transactionColumns: string[] = ['type', 'category', 'details', 'amount', 'date'];
 
+  constructor(
+    private readonly importService: ImportService,
+    private snackBar: MatSnackBar,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.importId = params['id'];
+
+      if (this.importId !== undefined) {
+        this.importService
+          .getImportById(this.importId)
+          .subscribe(model => {
+            this.importModel = model;
+            this.importForm.patchValue(this.importModel);
+          });
+      }
+    });
+  }
 }
--- src/expense-tracking-ui/src/app/models/import.model.ts
+++ src/expense-tracking-ui/src/app/models/import.model.ts
@@ -1,6 +1,9 @@
+import { Transaction } from "./transaction.model";
+
 export interface Import {
     id?: string;
     name: string;
     layout: string;
     createdAt: string;
+    transactions: Transaction[] 
 }
