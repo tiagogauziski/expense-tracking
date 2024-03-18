6a23e1bb30c57e8591a251fb1db55c4169966b5e
Tiago Gauziski <tiago.gauziski@gmail.com>
2024-03-18 22:24:48 +1300

    testing with dynamic linq queries.

--- src/api/Expense.Tracking.Api.Tests/DatabaseTests.cs
+++ src/api/Expense.Tracking.Api.Tests/DatabaseTests.cs
@@ -2,6 +2,7 @@
 using Expense.Tracking.Api.Domain.Models;
 using Expense.Tracking.Api.Infrastrucure.Database;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
 
 namespace Expense.Tracking.Api.Tests;
 
@@ -29,6 +30,7 @@ public class DatabaseTests
 
         var import = new Import()
         {
+            Name = "unittest",
             Layout = nameof(BankCsvLayoutEngine),
             Transactions = transactions.ToList()
         };
@@ -41,5 +43,23 @@ public class DatabaseTests
 
         Assert.Single(imports);
         Assert.Equal(97, imports.First().Transactions.Count);
+    }
+
+    [Fact]
+    public async Task ApplyImportRules()
+    {
+        var importEngine = new BankCsvLayoutEngine();
+
+        var filePath = @"C:\Dev\TransactionData\06-0222-0838845-00_Transactions_2024-01-14_2024-02-12.csv";
+        var transactions = await importEngine.Execute(File.OpenRead(filePath));
+
+
+        IQueryable<ImportTransaction> query = transactions.AsQueryable();
+
+        string userInput = "Type == \"Automatic Payment\" && Details == \"Faceup Rent\"";
+        query = query.Where(userInput);
+
+        // Execute the combined query
+        IEnumerable<ImportTransaction> results = query.ToList();
     }
 }
