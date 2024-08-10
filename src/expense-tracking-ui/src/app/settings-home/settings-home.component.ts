bac7e3f52b49b4347714e85973c300fc4efebad3
Tiago Gauziski <tiago.gauziski@gmail.com>
2024-08-10 12:29:39 +1200

    setup layout for settings page.

--- src/expense-tracking-ui/src/app/settings-home/settings-home.component.ts
+++ src/expense-tracking-ui/src/app/settings-home/settings-home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-settings-home',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, RouterOutlet],
   templateUrl: './settings-home.component.html',
   styleUrl: './settings-home.component.css'
 })
